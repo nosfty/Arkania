fab16be3eba6e037757d10311fb6a0cffb259b2a
NOSFTY <unsftyghub@gmail.com>
2024-08-11 01:31:00 +0300

    Add files via upload

---
+++ _items.py
@@ -0,0 +1,6 @@
+from _Classes import *
+
+resource_item = [
+    ResourceItem(None, "Stick", "Common", "Wood"),
+    ResourceItem(None, "Iron Ore", "Common", "Ore")
+    ]
---
+++ main.py
@@ -0,0 +1,16 @@
+import random
+from _items import  resource_item
+from _Classes import *
+
+
+
+
+# The game cannot be launched and run at this stage of development.
+
+
+
+
+
+
+
+
