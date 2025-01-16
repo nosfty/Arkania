c5ebac53e42ca6c7ca043c4bbfc082e2bc41231d
NOSFTY <unsftyghub@gmail.com>
2025-01-16 21:50:13 +0300

    Add files via upload

---
+++ core/_characters.py
@@ -0,0 +1,22 @@
+from core._Races import Races, _Races
+
+
+
+class Character:
+    def __init__(self, Name:str, Lvl:int, Race:Races, Gender:str, Exp:int):
+        self.name = Name
+        self.lvl = Lvl
+        self.race = Race
+        self.gender = Gender
+        self.exp = Exp
+
+
+
+
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,17 @@
+import random
+from core._items import  *
+from core._Races import  *
+from core._Inventory import  *
+
+
+
+
+# The game cannot be launched and run at this stage of development.
+
+
+
+
+
+
+
+
